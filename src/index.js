3e74213564eed5255eceddc83c0e5c794dabbb29
Vany <vany.ikuze@student.moringaschool.com>
2023-06-21 16:07:24 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  document.addEventListener("DOMContentLoaded", () => {
+    // your code here
+    let form = document.querySelector("form")
+    form.addEventListener("submit", (e) =>{
+      e.preventDefault()
+      activeTodo(e.target.new-task-description.value);
+      form.reset()})
+  });
+  
+  function activeTodo(todo){
+    let li = document.createElement("li")
+    let btn = document.createElement("button")
+    btn.addEventListener("click", deleteTodo)
+    btn.textContent = "x"
+    li.textContent = `${todo}`
+    li.appendChild(btn)
+    console.log(li)
+    document.querySelector("#tasks").appendChild(li)
+  }
+  function deleteTodo(e){
+    e.target.parentNode.remove()
+  }
